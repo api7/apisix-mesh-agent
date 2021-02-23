8e0976fab262da375c4ce7f430d362d49853de1e
Alex Zhang <zchao1995@gmail.com>
2021-02-23 14:54:35 +0800

    chore: add upstream adaptor (#11)
    related issue: #8

--- pkg/adaptor/xds/v3/types.go
+++ pkg/adaptor/xds/v3/types.go
@@ -1,20 +1,41 @@
 package v3
 
 import (
+	"errors"
+
+	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
+
 	"github.com/api7/apisix-mesh-agent/pkg/config"
 	"github.com/api7/apisix-mesh-agent/pkg/log"
 	"github.com/api7/apisix-mesh-agent/pkg/types/apisix"
+	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+)
+
+var (
+	// ErrRequireFurtherEDS means the translation of Cluster is not complete
+	// since it depends on EDS to fetch the load assignment (endpoints).
+	// Once this error was given, the Cluster should keep invisible until
+	// the EDS config arrived.
+	ErrRequireFurtherEDS = errors.New("required further EDS config")
+	// ErrFeatureNotSupportedYet means a non-supported feature exists in the
+	// xDS resource so the Adaptor goes ahead.
+	ErrFeatureNotSupportedYet = errors.New("feature not supported yet")
 )
 
 // Adaptor translates xDS resources like Route, Cluster
 // to the equivalent configs in Apache APISIX.
+// WARNING: not all fields are translated, only the necessary parts are used, others
+// can be added in the future.
 type Adaptor interface {
-	// TranslateRouteConfiguration translate a RouteConfiguration to a series APISIX
+	// TranslateRouteConfiguration translates a RouteConfiguration to a series APISIX
 	// Routes.
-	// WARNING: not all fields are translated, only the necessary parts are used, others
-	// can be added in the future.
 	TranslateRouteConfiguration(*routev3.RouteConfiguration) ([]*apisix.Route, error)
+	// TranslateCluster translates a Cluster to an APISIX Upstreams.
+	TranslateCluster(*clusterv3.Cluster) (*apisix.Upstream, error)
+	// TranslateClusterLoadAssignment translate the ClusterLoadAssignement resources to APISIX
+	// Upstream Nodes.
+	TranslateClusterLoadAssignment(*endpointv3.ClusterLoadAssignment) ([]*apisix.Node, error)
 }
 
 type adaptor struct {
@@ -26,7 +47,7 @@ func NewAdaptor(cfg *config.Config) (Adaptor, error) {
 	logger, err := log.NewLogger(
 		log.WithOutputFile(cfg.LogOutput),
 		log.WithLogLevel(cfg.LogLevel),
-		log.WithContext("xds_adaptor"),
+		log.WithContext("xds_v3_adaptor"),
 	)
 	if err != nil {
 		return nil, err
--- pkg/provisioner/xds/file_test.go
+++ pkg/provisioner/xds/file_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/api7/apisix-mesh-agent/pkg/id"
+
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+
+	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
+
+	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+
 	"github.com/golang/protobuf/ptypes/wrappers"
 
 	xdsv3 "github.com/api7/apisix-mesh-agent/pkg/adaptor/xds/v3"
@@ -176,12 +184,51 @@ func TestFileProvisionerGenerateEventsFromDiscoveryResponse(t *testing.T) {
 			},
 		},
 	}
-	var opaque any.Any
+	c := &clusterv3.Cluster{
+		Name:     "httpbin.default.svc.cluster.local",
+		LbPolicy: clusterv3.Cluster_ROUND_ROBIN,
+		LoadAssignment: &endpointv3.ClusterLoadAssignment{
+			ClusterName: "httpbin.default.svc.cluster.local",
+			Endpoints: []*endpointv3.LocalityLbEndpoints{
+				{
+					LbEndpoints: []*endpointv3.LbEndpoint{
+						{
+							HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+								Endpoint: &endpointv3.Endpoint{
+									Address: &corev3.Address{
+										Address: &corev3.Address_SocketAddress{
+											SocketAddress: &corev3.SocketAddress{
+												Protocol: corev3.SocketAddress_TCP,
+												Address:  "10.0.3.11",
+												PortSpecifier: &corev3.SocketAddress_PortValue{
+													PortValue: 8000,
+												},
+											},
+										},
+									},
+								},
+							},
+							LoadBalancingWeight: &wrappers.UInt32Value{
+								Value: 100,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	var (
+		opaque  any.Any
+		opaque2 any.Any
+	)
 	opaque.TypeUrl = "type.googleapis.com/" + string(rc.ProtoReflect().Descriptor().FullName())
 	assert.Nil(t, anypb.MarshalFrom(&opaque, rc, proto2.MarshalOptions{}))
+	opaque2.TypeUrl = "type.googleapis.com/" + string(c.ProtoReflect().Descriptor().FullName())
+	assert.Nil(t, anypb.MarshalFrom(&opaque2, c, proto2.MarshalOptions{}))
+
 	dr := &discoveryv3.DiscoveryResponse{
 		VersionInfo: "0",
-		Resources:   []*any.Any{&opaque},
+		Resources:   []*any.Any{&opaque, &opaque2},
 	}
 
 	cfg := &config.Config{
@@ -223,4 +270,85 @@ func TestFileProvisionerHandleFileEvent(t *testing.T) {
 	}
 	assert.Len(t, events, 1)
 	close(stopCh)
+}
+
+func TestFileProvisionerProcessClusterV3(t *testing.T) {
+	c := &clusterv3.Cluster{
+		Name:     "httpbin.default.svc.cluster.local",
+		LbPolicy: clusterv3.Cluster_ROUND_ROBIN,
+		LoadAssignment: &endpointv3.ClusterLoadAssignment{
+			ClusterName: "httpbin.default.svc.cluster.local",
+			Endpoints: []*endpointv3.LocalityLbEndpoints{
+				{
+					LbEndpoints: []*endpointv3.LbEndpoint{
+						{
+							HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+								Endpoint: &endpointv3.Endpoint{
+									Address: &corev3.Address{
+										Address: &corev3.Address_SocketAddress{
+											SocketAddress: &corev3.SocketAddress{
+												Protocol: corev3.SocketAddress_TCP,
+												Address:  "10.0.3.11",
+												PortSpecifier: &corev3.SocketAddress_PortValue{
+													PortValue: 8000,
+												},
+											},
+										},
+									},
+								},
+							},
+							LoadBalancingWeight: &wrappers.UInt32Value{
+								Value: 100,
+							},
+						},
+						{
+							HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+								Endpoint: &endpointv3.Endpoint{
+									Address: &corev3.Address{
+										Address: &corev3.Address_SocketAddress{
+											SocketAddress: &corev3.SocketAddress{
+												Protocol: corev3.SocketAddress_TCP,
+												Address:  "10.0.3.12",
+												PortSpecifier: &corev3.SocketAddress_PortValue{
+													PortValue: 8000,
+												},
+											},
+										},
+									},
+								},
+							},
+							LoadBalancingWeight: &wrappers.UInt32Value{
+								Value: 80,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	var opaque any.Any
+	opaque.TypeUrl = "type.googleapis.com/" + string(c.ProtoReflect().Descriptor().FullName())
+	assert.Nil(t, anypb.MarshalFrom(&opaque, c, proto2.MarshalOptions{}))
+	cfg := &config.Config{
+		LogLevel:  "debug",
+		LogOutput: "stderr",
+	}
+	adaptor, err := xdsv3.NewAdaptor(cfg)
+	assert.Nil(t, err)
+	p := &xdsFileProvisioner{
+		logger:    log.DefaultLogger,
+		v3Adaptor: adaptor,
+		state:     make(map[string]*resourceManifest),
+	}
+	upstreams := p.processClusterV3(&opaque)
+	assert.Len(t, upstreams, 1)
+	assert.Equal(t, upstreams[0].Name, "httpbin.default.svc.cluster.local")
+	assert.Equal(t, upstreams[0].Id.GetStrVal(), id.GenID(upstreams[0].Name))
+	assert.Len(t, upstreams[0].Nodes, 2)
+	assert.Equal(t, upstreams[0].Nodes[0].Host, "10.0.3.11")
+	assert.Equal(t, upstreams[0].Nodes[0].Port, int32(8000))
+	assert.Equal(t, upstreams[0].Nodes[0].Weight, int32(100))
+	assert.Equal(t, upstreams[0].Nodes[1].Host, "10.0.3.12")
+	assert.Equal(t, upstreams[0].Nodes[1].Port, int32(8000))
+	assert.Equal(t, upstreams[0].Nodes[1].Weight, int32(80))
 }
