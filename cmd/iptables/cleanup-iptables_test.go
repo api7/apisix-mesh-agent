c755256b5752da1a9a7df174d132e98ae67c39be
Chao Zhang <zchao1995@gmail.com>
2021-03-09 16:41:48 +0800

    test: add unit test cases

---
+++ cmd/iptables/cleanup-iptables_test.go
@@ -0,0 +1,36 @@
+package iptables
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCleanupIptables(t *testing.T) {
+	f, err := ioutil.TempFile("./", "iptables-cleanup.*")
+	assert.Nil(t, err)
+	defer func() {
+		assert.Nil(t, f.Close())
+		assert.Nil(t, os.Remove(f.Name()))
+	}()
+	rawStdout := os.Stdout
+	os.Stdout = f
+	cleanup(true)
+	os.Stdout = rawStdout
+
+	data, err := ioutil.ReadFile(f.Name())
+	assert.Nil(t, err)
+
+	expect := `iptables -t nat -D PREROUTING -p tcp -j APISIX_INBOUND
+iptables -t nat -D OUTPUT -p tcp -j OUTPUT
+iptables -t nat -F APISIX_INBOUND
+iptables -t nat -X APISIX_INBOUND
+iptables -t nat -F OUTPUT
+iptables -t nat -X OUTPUT
+iptables -t nat -F APISIX_REDIRECT
+iptables -t nat -X APISIX_REDIRECT
+`
+	assert.Equal(t, expect, string(data))
+}
