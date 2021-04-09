9cd044e1222e4adc7c1781380d2314ae7cce8a7f
Chao Zhang <zchao1995@gmail.com>
2021-04-09 14:52:44 +0800

    test: add unit test cases

---
+++ pkg/adaptor/xds/v3/listener_test.go
@@ -0,0 +1,98 @@
+package v3
+
+import (
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+
+	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+
+	listenerv3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	hcmv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	xdswellknown "github.com/envoyproxy/go-control-plane/pkg/wellknown"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	"github.com/api7/apisix-mesh-agent/pkg/log"
+)
+
+func TestCollectRouteNamesAndConfigs(t *testing.T) {
+	a := &adaptor{logger: log.DefaultLogger}
+
+	var (
+		any1 anypb.Any
+		any2 anypb.Any
+		any3 anypb.Any
+	)
+
+	f1 := &hcmv3.HttpConnectionManager{
+		RouteSpecifier: &hcmv3.HttpConnectionManager_Rds{
+			Rds: &hcmv3.Rds{
+				RouteConfigName: "route1",
+			},
+		},
+	}
+	f2 := &hcmv3.HttpConnectionManager{
+		RouteSpecifier: &hcmv3.HttpConnectionManager_Rds{
+			Rds: &hcmv3.Rds{
+				RouteConfigName: "route2",
+			},
+		},
+	}
+	f3 := &hcmv3.HttpConnectionManager{
+		RouteSpecifier: &hcmv3.HttpConnectionManager_RouteConfig{
+			RouteConfig: &routev3.RouteConfiguration{
+				Name: "route3",
+				VirtualHosts: []*routev3.VirtualHost{
+					{
+						Name: "v1",
+						Routes: []*routev3.Route{
+							{
+								Name: "route1",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	assert.Nil(t, anypb.MarshalFrom(&any1, f1, proto.MarshalOptions{}))
+	assert.Nil(t, anypb.MarshalFrom(&any2, f2, proto.MarshalOptions{}))
+	assert.Nil(t, anypb.MarshalFrom(&any3, f3, proto.MarshalOptions{}))
+
+	listener := &listenerv3.Listener{
+		Name: "listener1",
+		FilterChains: []*listenerv3.FilterChain{
+			{
+				Filters: []*listenerv3.Filter{
+					{
+						Name: xdswellknown.HTTPConnectionManager,
+						ConfigType: &listenerv3.Filter_TypedConfig{
+							TypedConfig: &any1,
+						},
+					},
+					{
+						Name: xdswellknown.HTTPConnectionManager,
+						ConfigType: &listenerv3.Filter_TypedConfig{
+							TypedConfig: &any2,
+						},
+					},
+					{
+						Name: xdswellknown.HTTPConnectionManager,
+						ConfigType: &listenerv3.Filter_TypedConfig{
+							TypedConfig: &any3,
+						},
+					},
+				},
+			},
+		},
+	}
+	rdsNames, staticConfigs, err := a.CollectRouteNamesAndConfigs(listener)
+	assert.Nil(t, err)
+	assert.Equal(t, rdsNames, []string{"route1", "route2"})
+	assert.Len(t, staticConfigs, 1)
+	assert.Equal(t, staticConfigs[0].Name, "route3")
+	assert.Len(t, staticConfigs[0].VirtualHosts, 1)
+	assert.Equal(t, staticConfigs[0].VirtualHosts[0].Name, "v1")
+}
